eeda160cad860eed5a2c7c4a3e2761f80ded9876
nekattrin <negreeva.katerina@gmail.com>
2024-01-07 00:20:04 +0300

    done

--- FriendFunctions&Overloads.cpp
+++ FriendFunctions&Overloads.cpp
@@ -1,20 +1,147 @@
-﻿// FriendFunctions&Overloads.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿//17. Создать класс Complex, реализующий комплексное число.В классе
+//перегрузить операторы унарный « + », возвращающий модуль комплексного
+//числа(тип float), бинарный оператор « + », возвращающий сумму
+//комплексных чисел(тип Complex), бинарный оператор « + », который
+//добавляет к комплексному числу некоторое вещественное число.В этом
+//случае операторная функция получает входным параметром вещественное
+//число и возвращает объект типа Complex.Создать методы доступа к полям
+//класса.Реализовать конструкторы(по умолчанию, с параметрами,
+//	копирования).Реализовать friend - функции для операторов ввода / вывода в
+//	поток для созданного класса
+
+
+
+//Почему возникает ошибка сборки если менять местами кейсы????!!!
 
 #include <iostream>
+#include<math.h>
+//#include <complex>
+
+using namespace std;
+
+
+class Complex {
+	float realPart;
+	float imaginaryPart;
+
+public:
+	Complex(float realPart = 0, float imaginaryPart = 0);
+	friend void setData(float, float, Complex&);
+	friend void showData(Complex&);
+	float operator +();
+	Complex operator +(Complex);
+	Complex operator +(float);
+};
+
+Complex::Complex(float realPart, float imaginaryPart)
+{
+	this->realPart = realPart;
+	this->imaginaryPart = imaginaryPart;
+}
+
+void setData(float realPart, float imaginaryPart, Complex& object)
+{
+	object.realPart = realPart;
+	object.imaginaryPart = imaginaryPart;
+}
+
+void showData(Complex& object)
+{
+	cout << "current number: " << object.realPart << " + " << object.imaginaryPart << "i" << endl << endl;
+}
+
+float Complex::operator+()
+{
+	float module;
+	module = realPart * realPart + imaginaryPart * imaginaryPart;
+	module = sqrt(module);
+	return module;
+}
+
+Complex Complex:: operator+(Complex	 object)
+{
+	this->realPart += object.realPart;
+	this->imaginaryPart += object.imaginaryPart;
+	return *this;
+}
+
+Complex Complex:: operator+(float real)
+{
+	this->realPart += real;
+	return *this;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float realPart, imaginaryPart, module;
+	bool finish = false;
+	int choice;
+	cout << "enter a complex number" << endl;
+	cout << "enter a real part:  ";
+	cin >> realPart;
+	cout << "enter an imaginary part:  ";
+	cin >> imaginaryPart;
+
+	Complex ComplexNumberMain;
+	setData(realPart, imaginaryPart, ComplexNumberMain);
+	showData(ComplexNumberMain);
+
+	/*float real1;
+	cout << "enter real1:  ";
+	cin >> real1;
+	ComplexNumberMain = ComplexNumberMain + real1;
+	showData(ComplexNumberMain);
+	*/
+
+
+	do
+	{
+		cout << "What are you going to do:\n 1 - see the module of the complex number\n"
+			" 2 - add another complex number to your complex number\n 3 - add a real number to your complex number\n"
+			" 4 - finish the program\n";
+		cin >> choice;
+
+		switch (choice)
+		{
+
+		case 1:
+			module = +ComplexNumberMain;
+			cout << "Module of the number is: " << module << endl << endl;
+			break;
+
+		case 4:
+			cout << "Goodbye\n";
+			finish = true;
+			break;
+
+		case 3:
+			float real1;
+			cout << "Enter a real number:  ";
+			cin >> real1;
+			ComplexNumberMain = ComplexNumberMain + real1;
+			showData(ComplexNumberMain);
+			break;
+
+		case 2:
+			float realPart1, imaginaryPart1;
+			cout << "Enter a complex numer\n";
+			cout << "enter a real part:  ";
+			cin >> realPart1;
+			cout << "enter an imaginary part:  ";
+			cin >> imaginaryPart1;
+			Complex ComplexNumber1;
+			setData(realPart1, imaginaryPart1, ComplexNumber1);
+			ComplexNumberMain = ComplexNumberMain + ComplexNumber1;
+
+			showData(ComplexNumberMain);
+			break;
+
+
+		}
+	} while (finish != true);
+
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+
